61e8b0c43bf917973eb9ff220757919fba4fb160
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2017-03-06 10:24:16 -0800

    install: add tap

--- agshoes-install.sh
+++ agshoes-install.sh
@@ -3,4 +3,5 @@
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew tap josephholsten/josephholsten
 brew install josephholsten/agshoes
