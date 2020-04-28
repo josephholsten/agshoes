95311a157d9898b5a7a67020dee650541ee78723
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2020-04-28 09:04:59 -0700

    install, absent: use bash homebrew installer

--- agshoes-install.sh
+++ agshoes-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 brew tap josephholsten/josephholsten
 brew install josephholsten/josephholsten/agshoes
